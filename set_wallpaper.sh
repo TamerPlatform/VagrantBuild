44486267b371f951f93c00ba20bc71123be940aa
Anant Shrivastava <anant@anantshri.info>
2016-05-25 14:20:43 +0530

    customizing wallpapers across the board, grub, login screen, and desktop

--- set_wallpaper.sh
+++ set_wallpaper.sh
@@ -5,3 +5,7 @@ PID=`pgrep marco`
 export DBUS_SESSION_BUS_ADDRESS=`grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-`
 # set proper wallpaper
 gsettings set org.mate.background picture-filename "/usr/share/backgrounds/tamer-black1.png"
+sudo cp /vagrant/tamer-noninterlaced.png /boot/grub/tamer.png
+sudo update-grub
+sudo sed -i "s/background=.*/background=\/usr\/share\/backgrounds\/tamer-black1.png/g" /etc/lightdm/lightdm-gtk-greeter.conf
+echo "" > ~/.zsh_history
